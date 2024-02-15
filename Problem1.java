7b38d0ea3a09e658266cd80e198c11ad2c6e7e3f
Harsh Malik <116074963+harshmalik24@users.noreply.github.com>
2024-02-15 05:38:09 +0530

    Add files via upload

--- Problem1.java
+++ Problem1.java
@@ -6,7 +6,7 @@ public class Problem1 {
         int num = sc.nextInt();                   // taking user input
         int digit = 0;                            // initialising variable digit to store the number to digits in the integer
         num = Math.abs(num);                      // method to return absolute or positive value in case integer is negative
-        while (num>0) {                           // loop to calculate number of digits
+        while (num>0) {                           // loop to calculate number of digits in integer
             num /= 10;
             digit++;
         }
