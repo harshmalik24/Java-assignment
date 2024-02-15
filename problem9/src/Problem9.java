15ff24be01d75b1cf89f6f6f05831b66997999c6
Harsh Malik <116074963+harshmalik24@users.noreply.github.com>
2024-02-16 03:06:31 +0530

    Update Problem9.java

--- problem9/src/Problem9.java
+++ problem9/src/Problem9.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 public class Problem9 {
-    public static boolean specialDigit(int number){
-        if (number >= 10 && number<=99){
-            int tensDigit = number/10;
-            int unitsDigit = number%10;
-            int sum = tensDigit + unitsDigit + (tensDigit * unitsDigit);
-            return sum == number;
+    public static boolean specialInteger(int number){
+        if (number >= 10 && number<=99){                                             // condition to ensure number is 2 digit only
+            int tensDigit = number/10;                                               // finding tens place digit
+            int unitsDigit = number%10;                                              // finding units place digit
+            
+            // checking if number is special integer or not
+            int sum = tensDigit + unitsDigit + (tensDigit * unitsDigit); 
+            return sum == number;                                                    // will return true only when the number is 2 digit special integer
         }
         else{
             return false;
@@ -15,7 +17,7 @@ public class Problem9 {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter the 2 digit number: ");
         int number= sc.nextInt();
-        if (specialDigit(number)){
+        if (specialInteger(number)){                                                  // function will return true if number is 2 digit special number
             System.out.println(number + " is a two-digit special integer");
         }
         else{
